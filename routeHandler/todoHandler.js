81a14b8ac9af65c0e078e5c1f71cebef6ae64777
sohelreza <61862128+sohelreza@users.noreply.github.com>
2022-04-21 00:57:14 +0600

    todo update done

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
+const todoSchema = require("../schemas/todoSchema");
+const Todo = new mongoose.model("Todo", todoSchema);
 
 // GET ALL THE TODOS
 router.get("/", async (req, res) => {});
@@ -8,13 +11,60 @@ router.get("/", async (req, res) => {});
 router.get("/:id", async (req, res) => {});
 
 // POST A TODO
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  const newTodo = new Todo(req.body);
+
+  await newTodo.save((err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todo was inserted successfully!",
+      });
+    }
+  });
+});
 
 // POST MULTIPLE TODO
-router.post("/all", async (req, res) => {});
+router.post("/all", async (req, res) => {
+  await Todo.insertMany(req.body, (err) => {
+    if (err) {
+      res.status(500).json({
+        error: "There was a server side error!",
+      });
+    } else {
+      res.status(200).json({
+        message: "Todos were inserted successfully",
+      });
+    }
+  });
+});
 
 // PUT TODO
-router.put("/:id", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+  const result = await Todo.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      $set: { status: "active" },
+    },
+    { new: true, useFindAndModify: false },
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          error: "There was a server side error!",
+        });
+      } else {
+        res.status(200).json({
+          message: "Todo was updated successfully",
+        });
+      }
+    }
+  );
+
+  console.log(result);
+});
 
 // DELETE TODO
 router.delete("/:id", async (req, res) => {});
