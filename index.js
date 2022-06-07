94f6f3e8a12fea527483ac82bdc998ba20a74539
sohelreza <61862128+sohelreza@users.noreply.github.com>
2022-06-08 02:16:00 +0600

    jwt completed

--- index.js
+++ index.js
@@ -10,10 +10,13 @@
 // Dependencies
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const todoHandler = require("./routeHandler/todoHandler");
+const userHandler = require("./routeHandler/userHandler");
 
 // express app initialization
 const app = express();
+dotenv.config();
 app.use(express.json());
 
 // database connection with mongoose
@@ -24,14 +27,18 @@ mongoose
 
 // application routes
 app.use("/todo", todoHandler);
+app.use("/user", userHandler);
 
 // default error handler
-function errorHandler(err, req, res, next) {
+const errorHandler = (err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
   }
+
   res.status(500).json({ error: err });
-}
+};
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("app listening at port 3000");
--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const router = express.Router();
 const todoSchema = require("../schemas/todoSchema");
 const Todo = new mongoose.model("Todo", todoSchema);
+const checkLogin = require("../middlewares/checkLogin");
 
 // GET ALL THE TODOS
-router.get("/", (req, res) => {
+router.get("/", checkLogin, (req, res) => {
   Todo.find({ status: "active" })
     .select({
       _id: 0,
